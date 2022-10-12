ba9d67973d67624f16069a8adde6688d550be196
Your Name <ferse.nattapong@gmail.com>
2022-10-13 00:05:11 +0700

    decorate 13/10

--- MID_POST.js
+++ MID_POST.js
@@ -8,27 +8,33 @@ const valuesensorInput = document.querySelector("#sensor_value");
 
 function insertSubmit(event) {
   event.preventDefault();
-
   const user = {
-    st_id:idInput.value,
-    st_name:nameInput.value,
-    sensor_name:namesensorInput.value,
-    sensor_type:typesensorInput.value,
-    sensor_unit:unitsensorInput.value,
-    sensor_value:valuesensorInput.value,
+    st_id: idInput.value,
+    st_name: nameInput.value,
+    sensor_name: namesensorInput.value,
+    sensor_type: typesensorInput.value,
+    sensor_unit: unitsensorInput.value,
+    sensor_value: valuesensorInput.value,
   };
-  fetch("https://midterm-exam-010723313-2022.herokuapp.com/student_post",{
+  fetch("https://midterm-exam-010723313-2022.herokuapp.com/student_post", {
     method: 'POST',
     headers: {
-        'Content-Type':'application/json;charset=utf-8'
+      'Content-Type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify(user)
   })
     .then((res) => res.json())
     .then((json) => {
     })
+
     .catch((error) => {
       console.log(error.message);
     });
+  idInput.value = '';
+  nameInput.value = '';
+  namesensorInput.value = '';
+  typesensorInput.value = '';
+  unitsensorInput.value = '';
+  valuesensorInput.value = '';
 }
 insertForm.addEventListener("submit", insertSubmit);
