64d7e2382079192424d55e0f6cb0405a74da6126
Your Name <ferse.nattapong@gmail.com>
2022-10-13 03:24:31 +0700

    change file name

--- js_flow.js
+++ js_flow.js
@@ -16,32 +16,33 @@ function showFT(FT) {
     placeholder.innerHTML += rows;
   });
 }
-function filterSearch(){
+function filterSearch() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("dataTable");
   tr = document.getElementsByTagName("tr");
-  
-  for(i = 0;i<tr.length;i++){
+
+  for (i = 0; i < tr.length; i++) {
     td_name = tr[i].getElementsByTagName("td")[2];
-    if(td_name){
+    if (td_name) {
       txtValue = td_name.textContent || td_name.innerText;
-      if(txtValue.toUpperCase().indexOf(filter)>-1){
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
-      }else{
+      } else {
         tr[i].style.display = "none";
       }
     }
   }
 }
-fetch(
-  "https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer"
-)
-  .then((res) => res.json())
-  .then((FT) => {
-    showFT(FT);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+  fetch(
+    "https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer"
+  )
+    .then((res) => res.json())
+    .then((FT) => {
+      showFT(FT);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
